94b3514d8cb48bed0fc4df8056f97f101f2a2837
mkfreeman <michael.k.freeman@gmail.com>
2016-02-06 12:15:36 -0800

    Updated repo to accommodate changes to the mass shootings database

--- js/getData.js
+++ js/getData.js
@@ -1,10 +1,11 @@
+var test;
 $(function(){    
   
   // Function exectued when form is submitted
   var getData = function(apiKey) {
 
     // Use crossorigin.me to gain access to the .csv file
-    var filepath = 'https://crossorigin.me/http://shootingtracker.com/tracker/2015CURRENT.csv'
+    var filepath = 'https://crossorigin.me/http://www.gunviolencearchive.org/export-finished/download?uuid=2bde9dc6-f32c-46b2-b091-cefb1073fae5&filename=public%3A//export-06d8dd3b-26e6-4c22-9e9f-142cfb6bfa35.csv'
     var promises = []
     
     // Read data in with Papa.parse
@@ -12,16 +13,21 @@ $(function(){
       download: true,
       header: true,
       complete: function(results) {
-        
+        test = results;
         // Calculate lat/long for each data-point
         results.data.forEach(function(d, i){
-        
-          // Remove empty string property 
-          delete d['']
-
+          var keys = Object.keys(d)
           // Get city / state
-          d.city = d.Location.split(',')[0]
-          d.state = d.Location.split(', ')[1]
+          d.date = d['Incident Date']
+          d.city = d['City Or County']
+          d.state = d['State']
+          d.injured = d['# Injured']
+          d.killed = d['# Killed']
+
+          // Delete unnecessary keys
+          keys.forEach(function(key){
+            delete d[key]
+          })
 
           // Pass to getLatLong function to set object properties: push into promises array
           promises.push(getLatLong(d, apiKey))
