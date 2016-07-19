6b971443bc676cd9b76324697113ef85ad686174
mkfreeman <michael.k.freeman@gmail.com>
2016-07-19 18:21:05 -0400

    Updated to 2016 data

--- js/getData.js
+++ js/getData.js
@@ -1,55 +1,55 @@
 var test;
-$(function(){    
-  
+$(function(){
+
   // Function exectued when form is submitted
   var getData = function(apiKey) {
 
     // Use crossorigin.me to gain access to the .csv file
-    var filepath = 'https://crossorigin.me/http://www.gunviolencearchive.org/export-finished/download?uuid=2bde9dc6-f32c-46b2-b091-cefb1073fae5&filename=public%3A//export-06d8dd3b-26e6-4c22-9e9f-142cfb6bfa35.csv'
-    var promises = []
-    
+    var filepath = 'https://crossorigin.me/http://www.gunviolencearchive.org/export-finished/download?uuid=0484b316-f676-44bc-97ed-ecefeabae077&filename=public%3A//export-27a2429d-4db6-438a-b60b-265f67c6085c.csv';
+    var promises = [];
+
     // Read data in with Papa.parse
     Papa.parse(filepath, {
       download: true,
       header: true,
       complete: function(results) {
-        test = results;
+        test = $.extend({},results, true);
         // Calculate lat/long for each data-point
         results.data.forEach(function(d, i){
-          var keys = Object.keys(d)
+          var keys = Object.keys(d);
           // Get city / state
-          d.date = d['Incident Date']
-          d.city = d['City Or County']
-          d.state = d['State']
-          d.injured = d['# Injured']
-          d.killed = d['# Killed']
+          d.date = d['Incident Date'];
+          d.city = d['City Or County'];
+          d.state = d['State'];
+          d.injured = d['# Injured'];
+          d.killed = d['# Killed'];
 
           // Delete unnecessary keys
           keys.forEach(function(key){
-            delete d[key]
-          })
+            delete d[key];
+          });
 
           // Pass to getLatLong function to set object properties: push into promises array
-          promises.push(getLatLong(d, apiKey))
+          promises.push(getLatLong(d, apiKey));
 
-        })
-        
+        });
+
         // Download results once requests have returned
         $.when.apply($, promises).then(function() {
-          download(results.data)                
+          download(results.data);
         }, function() {
             // error occurred
-        });        
+        });
       }
-    })
+    });
 
-  }
-  
+  };
+
 
   // Function to get lat/long based on these data objects using openCageData
   var getLatLong = function(obj, apiKey) {
-    var q = encodeURI(obj.city + ',' + obj.state + ', United States')
-    
+    var q = encodeURI(obj.city + ',' + obj.state + ', United States');
+
     // Return a promise in order to wait for all requests to finish
     return $.ajax({
         url:'http://api.opencagedata.com/geocode/v1/json?q=' + q + '&key=' + apiKey,
@@ -57,16 +57,16 @@ $(function(){
         success:function(response) {
 
           // Set lat/lng of each data-point
-          if(response.results == undefined) return
-          obj.lat = response.results[0].geometry.lat
-          obj.lng = response.results[0].geometry.lng
-        }, 
+          if(response.results == undefined) return;
+          obj.lat = response.results[0].geometry.lat;
+          obj.lng = response.results[0].geometry.lng;
+        },
         error:function(error) {
-          console.log('error ', error)
+          console.log('error ', error);
         },
        dataType:"json"
-    }) 
-  }
+    });
+  };
 
   // Function to download the data to the client syntax from https://github.com/mholt/PapaParse/issues/175
   var download = function(data) {
@@ -84,23 +84,23 @@ $(function(){
       }
 
       // Change button
-      $('#download').find('span').css('display', 'none')
-      $('#download').find('text').text('Download')
-      
-    }
-  
+      $('#download').find('span').css('display', 'none');
+      $('#download').find('text').text('Download');
+
+    };
+
   // Assign event listener to button
   $("form").on('submit', function(){
-      
+
       // Change button
-      $('#download').find('span').css('display', 'inline')      
-      $('#download').find('text').text('Calculating lat/lng...')
+      $('#download').find('span').css('display', 'inline');
+      $('#download').find('text').text('Calculating lat/lng...');
 
       // Get api key
-      var input = $(this).find('input')      
-      var apiKey = input.val()
-      input.val('')
-      getData(apiKey)
-      return false
-  })
-})
+      var input = $(this).find('input');
+      var apiKey = input.val();
+      input.val('');
+      getData(apiKey);
+      return false;
+  });
+});
